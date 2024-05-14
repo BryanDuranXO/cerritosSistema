ccd664b10e4f05d123aa2ca470dbd44c3ac50cbc
BryanDuranXO <20223tn134@utez.edu.com>
2024-05-14 14:50:14 -0600

    spring

---
+++ back/CerritosBack/src/main/java/mx/edu/utez/CerritosBack/CerritosBackApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.CerritosBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerritosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerritosBackApplication.class, args);
+	}
+
+}
