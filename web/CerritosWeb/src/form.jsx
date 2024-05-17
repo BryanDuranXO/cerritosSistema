b0ceb2a9a94f4dd590d0042c6d5489c489316233
BryanDuranXO <20223tn134@utez.edu.com>
2024-05-16 22:21:01 -0600

    animaciones arregladas en inicio

--- web/CerritosWeb/src/form.jsx
+++ web/CerritosWeb/src/form.jsx
@@ -83,6 +83,7 @@ function Form() {
                     <button type="submit">Enviar</button>
                 </form>
             </div>
+
         </>
     );
 }
--- web/CerritosWeb/src/screens/inicio.jsx
+++ web/CerritosWeb/src/screens/inicio.jsx
@@ -1,15 +1,15 @@
+import { Fade } from "react-awesome-reveal";
 import CollapsibleExample from '../components/menu2';
 import { Navegacion } from '../components/nav';
 import '../css/inicio.css';
 import '../css/style.css';
-import { Fade } from "react-reveal";
 
 export const Inicio = () => {
     return (
         <>
             <CollapsibleExample />
             <div className="main-container">
-                <Fade left>
+                <Fade direction="left">
                     <div className="contenedor">
                         <div className="text-container">
                             <div className='texto'>
@@ -17,7 +17,9 @@ export const Inicio = () => {
                             </div>
                             <div className="secondary-text">
                                 <h3>Generando Momentos que recordarás toda la vida</h3>
-                                <p className='info'>Estamos comprometidos a hacer de su visita un recuerdo atesorable, por lo que todo nuestro empeñoestará enfocado en personalizar nuestros servicios a sus necesidades y expectativas.</p>
+                                <p className='info'>
+                                    Estamos comprometidos a hacer de su visita un recuerdo atesorable, por lo que todo nuestro empeño estará enfocado en personalizar nuestros servicios a sus necesidades y expectativas.
+                                </p>
                             </div>
                             <div className="boton">
                                 <button>Reserva ahora</button>
@@ -29,49 +31,52 @@ export const Inicio = () => {
                     </div>
                 </Fade>
 
-                <Fade right>
-    <div className="hotel-imagenes-container">
-        <div className="hotel">
-            <div className="texto">
-                <p className="tittle">Hotel</p>
-                <p className="info">
-                    ¿Buscas placer o negocios? Descubre el confort de nuestras villas con 18 habitaciones completamente equipadas. Disfruta de un descanso excepcional mientras contemplas nuestros extensos y encantadores jardines, llenos de historia y belleza.
-                </p>
-                <button className='btnHab'>Habitaciones</button>
-            </div>
-        </div>
-        <div className="imagenes">
-            <img src={'https://cerritosxochitepec.com/wp-content/uploads/2021/12/Home1.jpg'} alt="img" />
-        </div>
-    </div>
-</Fade>
+                <Fade direction="right">
+                    <div className="hotel-imagenes-container">
+                        <div className="hotel">
+                            <div className="texto">
+                                <p className="tittle">Hotel</p>
+                                <p className="info">
+                                    ¿Buscas placer o negocios? Descubre el confort de nuestras villas con 18 habitaciones completamente equipadas. Disfruta de un descanso excepcional mientras contemplas nuestros extensos y encantadores jardines, llenos de historia y belleza.
+                                </p>
+                                <button className='btnHab'>Habitaciones</button>
+                            </div>
+                        </div>
+                        <div className="imagenes">
+                            <img src={'https://cerritosxochitepec.com/wp-content/uploads/2021/12/Home1.jpg'} alt="img" />
+                        </div>
+                    </div>
+                </Fade>
 
-                <Fade right>
+                <Fade direction="right">
                     <div className="evt">
                         <div className="texto">
                             <p className="tittle">Eventos Sociales</p>
-                            <p className="info">Nuestras instalaciones para eventos te brindan hermosos jardines, lagos con vegetación exótica y vistas impresionantes, ¡la elección perfecta para ti y tus invitados!</p>
+                            <p className="info">
+                                Nuestras instalaciones para eventos te brindan hermosos jardines, lagos con vegetación exótica y vistas impresionantes, ¡la elección perfecta para ti y tus invitados!
+                            </p>
                         </div>
                     </div>
                 </Fade>
             </div>
 
-            
-            <Fade left>
-    <div className="hotel-imagenes-container">
-        <div className="hotel">
-            <div className="texto">
-                <p className="tittle">Espacios</p>
-                <p className="info">
-                Explora nuestros jardines amplios y exuberantes, tres piscinas con camastros, una cancha de tenis y un mini golf para disfrutar al máximo tu estadía.                </p>
-                <button className='btnHab'>Habitaciones</button>
-            </div>
-        </div>
-        <div className="imagenes">
-            <img src={'https://cerritosxochitepec.com/wp-content/uploads/2021/12/Home1.jpg'} alt="img" />
-        </div>
-    </div>
-</Fade>
+            <Fade direction="left">
+                <div className="hotel-imagenes-container">
+                    <div className="hotel">
+                        <div className="texto">
+                            <p className="tittle">Espacios</p>
+                            <p className="info">
+                                Explora nuestros jardines amplios y exuberantes, tres piscinas con camastros, una cancha de tenis y un mini golf para disfrutar al máximo tu estadía.
+                            </p>
+                            <button className='btnHab'>Habitaciones</button>
+                        </div>
+                    </div>
+                    <div className="imagenes">
+                        <img src={'https://cerritosxochitepec.com/wp-content/uploads/2021/12/Home1.jpg'} alt="img" />
+                    </div>
+                </div>
+            </Fade>
+
         </>
     );
 }
