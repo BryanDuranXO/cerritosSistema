627b37c7963dcf77ebd98772175c4ff164075440
Fernanda260504 <132949457+Fernanda260504@users.noreply.github.com>
2024-06-08 22:12:02 -0600

    Tablita pitera

---
+++ web/CerritosWeb/src/screens/reservasUsuario.jsx
@@ -0,0 +1,17 @@
+
+import '../css/eventos.css'
+
+const Inicio = () => {
+    return(
+        <>
+        
+
+            <div className="etiqueta">
+                Eventos Sociales
+            </div>
+
+        </>
+    );
+}
+
+export default Eventos;
