2020c02d721817e0d7cfe22ea56a2992c6d8c18f
BryanDuranXO <20223tn134@utez.edu.com>
2024-06-21 16:46:19 -0600

    crud personas completo

---
+++ back/CerritosBack/src/main/java/mx/edu/utez/CerritosBack/controller/Personas/PersonaController.java
@@ -0,0 +1,41 @@
+package mx.edu.utez.CerritosBack.controller.Personas;
+
+import com.fasterxml.jackson.core.PrettyPrinter;
+import lombok.AllArgsConstructor;
+import mx.edu.utez.CerritosBack.config.ApiResponse;
+import mx.edu.utez.CerritosBack.controller.Personas.DTO.PersonaDTO;
+import mx.edu.utez.CerritosBack.model.personas.PersonaBean;
+import mx.edu.utez.CerritosBack.service.Persona.PersonaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cerritos/persona")
+@CrossOrigin(origins = {"*"})
+@AllArgsConstructor
+public class PersonaController {
+
+    private final PersonaService personaService;
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAll(){
+        return new ResponseEntity<>(new ApiResponse(personaService.getAllPeople(), HttpStatus.OK, "todo bien"), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse> savePerson(@RequestBody PersonaDTO personaDTO){
+        return personaService.savePerson(personaDTO.toEntity());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updatePerson(@PathVariable("id") Long id, @RequestBody PersonaDTO dto){
+        dto.setId(id);
+        return personaService.updatePerson(dto.toUpdate());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse>deletePerson(@PathVariable("id") Long id){
+        return personaService.deletePerson(id);
+    }
+}
